b3c728f5de09486278060348d81ea59bb6809886
Mac <mac@mac.com>
2024-11-09 17:44:57 -0500

    Started

---
+++ loggerblocker/loggerblocker/loggerblockerApp.swift
@@ -0,0 +1,56 @@
+import SwiftUI
+import NetworkExtension
+
+class VPNManager: ObservableObject {
+    @Published var status: NEVPNStatus = .invalid
+    private var manager: NETunnelProviderManager?
+    
+    func setupVPN() {
+        NETunnelProviderManager.loadAllFromPreferences { [weak self] (managers, _) in
+            self?.manager = managers?.first ?? NETunnelProviderManager()
+            
+            let proto = NETunnelProviderProtocol()
+            proto.providerBundleIdentifier = "com.loggerblocker.loggerblocker.firewall"
+            proto.serverAddress = "127.0.0.1"
+            
+            self?.manager?.protocolConfiguration = proto
+            self?.manager?.isEnabled = true
+            
+            self?.manager?.saveToPreferences { _ in
+                try? self?.manager?.connection.startVPNTunnel()
+            }
+        }
+    }
+    
+    func stopVPN() {
+        manager?.connection.stopVPNTunnel()
+    }
+}
+
+struct MainView: View {
+    @StateObject private var vpnManager = VPNManager()
+    
+    var body: some View {
+        Button(action: {
+            vpnManager.setupVPN()
+        }) {
+            Text("Start VPN")
+        }
+        
+        Button(action: {
+            vpnManager.stopVPN()
+        }) {
+            Text("Stop VPN")
+        }
+    }
+}
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
+
